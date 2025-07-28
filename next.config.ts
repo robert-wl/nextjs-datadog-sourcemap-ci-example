3fba11bc38c6162abfc8e17143de8eb247e7a942
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2025-07-28 21:58:18 +0700

    feat: added app version and made it show up in page.tsx

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    env: {
+        NEXT_PUBLIC_APP_VERSION: `${process.env.NEXT_PUBLIC_ENVIRONMENT}-${process.env.VERCEL_GIT_COMMIT_SHA}`,
+    },
 };
 
 export default nextConfig;
